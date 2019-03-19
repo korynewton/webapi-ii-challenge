d6568709d0f3d420a4be1efae08d10940f187809
korynewton <korynewtondev@gmail.com>
2019-03-19 12:36:31 -0700

    posts-router connected for /api/posts methods

--- routes/posts-router.js
+++ routes/posts-router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    res.send("connnected")
+    res.send("router connected")
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
+const postsRouter = require('./routes/posts-router')
+
 const server = express();
 
-// server.use(express.json());
-
+server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send("connected")
 })
+
+server.use('/api/posts', postsRouter)
 
 module.exports = server;
