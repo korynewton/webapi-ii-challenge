fab25fc24d237d4d77bb011b2388c0c0e36a4831
korynewton <korynewtondev@gmail.com>
2019-03-19 13:07:47 -0700

    POST to /api/posts working to add new posts correctly

--- routes/posts-router.js
+++ routes/posts-router.js
@@ -33,4 +33,18 @@ router.get('/:id', (req, res) => {
         })
 })
 
+router.post('/', (req , res) => {
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        return res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+    db.insert(req.body)
+        .then(post => {
+            res.status(201).json(post)
+        })
+        .catch( err => {
+            res.status(500).json({ error: "There was an error while saving the post to the database" })
+        })
+})
+
 module.exports = router;
